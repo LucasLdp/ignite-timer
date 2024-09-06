15dd9eb261cb7ec5e194b286779612d5e57d2c36
Lucas <lucasdepauloldp1@gmail.com>
2024-09-05 22:41:42 -0300

    feat: create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+export function Home() {
+	return (
+		<div>
+			<form action="">
+				<label htmlFor="task">Vou trabalhar em</label>
+				<input type="text" name="" id="task" />
+
+				<label htmlFor="time">durante</label>
+				<input type="number" name="" id="time" />
+				
+				<span>Minutos</span>
+			</form>
+		</div>
+	);
+}
